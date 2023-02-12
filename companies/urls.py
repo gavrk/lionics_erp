407a9f781b7b6ce81a43474d4ee437c730e60373
Gavriil <gabrielko@yandex.ru>
2023-02-13 00:34:18 +0300

    index_loc ready

--- companies/urls.py
+++ companies/urls.py
@@ -13,11 +13,11 @@ urlpatterns = [
     path(r'register_loc/', views.CompanyLocCreate.as_view(), name='register_loc'),
 
     # /companies/detail_loc/some_id/
-    path('detail_loc/<int:companyloc_id>/', views.detail_loc, name='detail_loc'),
+    path('index_loc/<int:companyloc_id>/', views.detail_loc, name='detail_loc'),
 
     # /companies/register_int/
     path('register_int/', views.register_int, name='register_int'),
 
     # /companies/detail_int/some_id/
-    path('detail_int/<int:companyint_id>/', views.detail_int, name='detail_int'),
+    path('index_int/<int:companyint_id>/', views.detail_int, name='detail_int'),
 ]
