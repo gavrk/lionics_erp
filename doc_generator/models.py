1368aa20ef97bd5734632811743ab3f5c082ad0e
Gavriil <gabrielko@yandex.ru>
2023-02-19 20:37:40 +0300

    ForeignKey clash, seemingly need to create different models for own and client's companies

--- doc_generator/models.py
+++ doc_generator/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class StandardTesContract(models.Model):
+    agent = models.ForeignKey("companies.CompanyLoc", on_delete=models.CASCADE)
+    customer = models.ForeignKey("companies.CompanyLoc", on_delete=models.CASCADE)
+    contract_number = models.CharField(max_length=100, default='-')
+    contract_date = models.CharField(max_length=100, default='-')
+    contract_city = models.CharField(max_length=250, default='-')
+    valid_until = models.CharField(max_length=100, default='-')
+    blank = models.BooleanField(default=True)
+    stamped = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.contract_number
+
+    def get_absolute_url(self):
+        return reverse('index')
