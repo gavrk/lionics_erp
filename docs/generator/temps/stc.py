7c9b749b5685cbb8ee441dc0aea7e62441c841e5
Gavriil <gabrielko@yandex.ru>
2023-02-25 17:28:42 +0300

    Now opening xls

--- docs/generator/temps/stc.py
+++ docs/generator/temps/stc.py
@@ -1,9 +1,9 @@
 import pandas as pd
-
 import os
+
 settings_dir = os.path.dirname(__file__)
 GENERATOR_ROOT = os.path.abspath(os.path.dirname(settings_dir))
 
-content = pd.read_excel(GENERATOR_ROOT + '/contents/stc.xls', engine='openpyxl')
+content = pd.read_excel(GENERATOR_ROOT + '/contents/stc.xls')
 
 print(content)
